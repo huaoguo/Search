61720550795e25fe26f95f4d64e1b70cd8a0d75a
胡傲果 <huaoguo@gmail.com>
2013-01-21 00:04:32 +0800

    增加了一个读取html文件的方法，利用htmlparser实现，可以自动识别编码

--- src/search/utils/FileUtils.java
+++ src/search/utils/FileUtils.java
@@ -5,15 +5,28 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+import org.htmlparser.Parser;
+import org.htmlparser.util.ParserException;
 
 public class FileUtils {
 
-	public static String readFile(String filepath) throws IOException{
+	public static String readHtmlFile(String filepath) throws ParserException {
+		return new Parser(filepath).parse(null).toHtml();
+	}
+
+	public static String readFile(String filepath) throws IOException {
+		return readFile(filepath, "GBK");
+	}
+
+	public static String readFile(String filepath, String encoding) throws IOException {
 		File file = new File(filepath);
-		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file),
+				Charset.forName(encoding)));
 		String line = null;
 		StringBuilder sb = new StringBuilder();
-		while((line = br.readLine()) != null){
+		while ((line = br.readLine()) != null) {
 			sb.append(line + "\r\n");
 		}
 		br.close();
