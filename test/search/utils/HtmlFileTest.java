af8832a14aef49e8649c70719162b5df5262c032
胡傲果 <huaoguo@gmail.com>
2013-01-25 16:21:40 +0800

    写死的路径改为用Search.path常量

--- test/search/utils/HtmlFileTest.java
+++ test/search/utils/HtmlFileTest.java
@@ -2,12 +2,14 @@ package search.utils;
 
 import org.htmlparser.util.ParserException;
 
+import search.Search;
+
 import junit.framework.TestCase;
 
 public class HtmlFileTest extends TestCase{
 
 	public void testGetTitle() throws ParserException{
-		HtmlFile hf = new HtmlFile("file:///E:/workspace/heritrix-3.1.0/mirror/zywqs.blog.51cto.com/index.html");
+		HtmlFile hf = new HtmlFile(Search.path + "/already/zywqs.blog.51cto.com/index.html");
 		assertEquals("忘情水 曾经年少爱追梦 - 51CTO技术博客 - 领先的IT技术博客", hf.getTitle());
 	}
 }
