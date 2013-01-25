3c517c81efe6e2376100bd6dae2944b6645e48e8
胡傲果 <huaoguo@gmail.com>
2013-01-25 16:28:06 +0800

    dict_doc加入了tf2,tf3列

--- db/test/create_table.sql
+++ db/test/create_table.sql
@@ -3,4 +3,4 @@ create table dict (id integer primary key,value varchar(50),idf real);
 drop table if exists doc;
 create table doc (id integer primary key,title varchar(50),text text,url varchar(50));
 drop table if exists dict_doc;
-create table dict_doc (id integer primary key,dict_id integer,doc_id integer,tf integer);
+create table dict_doc (id integer primary key,dict_id integer,doc_id integer,tf integer,tf2 real,tf3 real);
