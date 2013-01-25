fc1323db91b17363ada98448c5106438a8757e23
胡傲果 <huaoguo@gmail.com>
2013-01-25 16:26:43 +0800

    加入了标题维度

--- src/search/Server.java
+++ src/search/Server.java
@@ -28,13 +28,6 @@ import search.utils.Xiangliang.XiangliangBuilder;
 public class Server {
 	private static final Logger logger = LogManager.getLogger(Server.class);
 
-	public static void main(String[] args) throws Exception {
-		List<Result> results = search("幽鬼出门装备");
-		for (Result result : results) {
-			System.out.println("doc: " + result.docId + ", cosineValue: " + result.cosineValue);
-		}
-	}
-
 	public static List<Result> search(String query) throws IOException, Exception, ClassNotFoundException, SQLException {
 		IKSegmenter seg = new IKSegmenter(new StringReader(query), false);
 		Lexeme lex = null;
@@ -42,18 +35,23 @@ public class Server {
 		KongjianBuilder kjBuilder = Kongjian.build();
 		while ((lex = seg.next()) != null) {
 			Dict dict = DBUtils.queryObject(Dict.class, "value='" + lex.getLexemeText() + "'");
+			if(dict == null){
+				continue;
+			}
 			logger.debug("word:" + dict.getValue() + ",idf:" + dict.getIdf());
 			dicts.add(dict);
 			kjBuilder.addWeidu(dict.getId());
+			kjBuilder.addWeidu("title." + dict.getId());
 		}
 		Kongjian kj = kjBuilder.toKongjian();
 		XiangliangBuilder xlBuilder = kj.buildXiangliang();
 		for (Dict dict : dicts) {
 			xlBuilder.setValue(dict.getId(), dict.getIdf());
+			xlBuilder.setValue("title." + dict.getId(), dict.getIdf());
 		}
 		Xiangliang xl = xlBuilder.toXiangliang();
 		StringBuilder sql = new StringBuilder();
-		sql.append("select doc.id doc_id,dict.id dict_id,dict.idf * dd.tf3 weight from dict_doc dd "
+		sql.append("select doc.id doc_id,dict.id dict_id,dict.idf * dd.tf3 weight,title_include from dict_doc dd "
 				+ "inner join dict on dd.dict_id = dict.id " + "inner join doc on dd.doc_id = doc.id "
 				+ "where dict.id in (");
 		for (Dict d : dicts) {
@@ -62,6 +60,7 @@ public class Server {
 		sql.deleteCharAt(sql.length() - 1);
 		sql.append(") order by doc.id,dict.id,weight desc");
 		logger.debug(sql);
+		long t = System.currentTimeMillis();
 		Class.forName("org.sqlite.JDBC");
 		Connection conn = DriverManager.getConnection("jdbc:sqlite:" + Conventions.getDBPath());
 		PreparedStatement pstmt = conn.prepareStatement(sql.toString());
@@ -72,13 +71,15 @@ public class Server {
 			r.docId = rs.getInt("doc_id");
 			r.dictId = rs.getInt("dict_id");
 			r.weight = rs.getDouble("weight");
+			r.titleInclude = rs.getBoolean("title_include");
 			records.add(r);
 		}
-		logger.debug("命中：" + records.size());
+		logger.debug("命中：" + records.size() + ",查询用时：" + (System.currentTimeMillis() - t) + "ms");
 		rs.close();
 		pstmt.close();
 		conn.close();
 
+		t = System.currentTimeMillis();
 		Record previous = null;
 		XiangliangBuilder xlBuilder2 = kj.buildXiangliang();
 		List<Result> results = new ArrayList<>();
@@ -91,8 +92,19 @@ public class Server {
 				xlBuilder2 = kj.buildXiangliang();
 			}
 			xlBuilder2.setValue(record.dictId, record.weight);
+			if(record.titleInclude){
+				double weight = 0d;
+				for (Dict dict : dicts) {
+					if(dict.getId().equals(record.dictId)){
+						weight = dict.getIdf();
+						break;
+					}
+				}
+				xlBuilder2.setValue("title." + record.dictId, weight);
+			}
 			previous = record;
 		}
+		logger.debug("计算余弦值，用时:" + (System.currentTimeMillis() - t) + "ms");
 		Collections.sort(results, new Comparator<Result>() {
 
 			@Override
@@ -121,4 +133,5 @@ class Record {
 	int docId;
 	int dictId;
 	double weight;
+	boolean titleInclude;
 }
